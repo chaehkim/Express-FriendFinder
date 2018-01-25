ba2e767644c1e51784b9396ce5ac3842bd7d5b35
unknown <chaekim@yahoo.com>
2018-01-25 12:55:26 -0500

    Final

---
+++ app/data/friends.js
@@ -0,0 +1,163 @@
+var friends = [
+  {
+    name: "Halle Berry",
+    photo: "http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/actors/halle-berry-19.3.jpg",
+    scores: [
+      "1",
+      "5",
+      "5",
+      "1",
+      "1",
+      "5",
+      "1",
+      "5",
+      "5",
+      "1"
+    ]
+  },
+  {
+    name: "Angelina Jolie",
+    photo: "https://www.instagram.com/p/BUpHTIDhhVY/?utm_source=ig_embed&utm_campaign=embed_legacy",
+    scores: [
+      "5",
+      "5",
+      "1",
+      "5",
+      "1",
+      "3",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Mila Kunis",
+    photo: "https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-img.instyle.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F684xflex%2Fpublic%2Fimages%2F2017%2F06%2F062717-mila-kunis-lead.jpg%3Fitok%3Dvekibvp8&w=800&q=85",
+    scores: [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  },
+  {
+    name: "Julia Roberts",
+    photo: "https://www.usmagazine.com/wp-content/uploads/1251226976_julia_roberts_290x402.jpg?w=225&h=400&crop=1",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Catherine Zeta-Jones",
+    photo: "http://sm.askmen.com/askmen_me/photo/c/catherine-zeta-jones/catherine-zeta-jones_pqsy.jpg",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Margot Robbie",
+    photo: "https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/33/1471280972-hbz-margot-robbie-list-embed3.jpg?crop=1.0xw:1xh;center,top&resize=768:*",
+    scores: [
+      "1",
+      "5",
+      "5",
+      "1",
+      "5",
+      "5",
+      "5",
+      "5",
+      "1",
+      "1"
+    ]
+  },
+    {
+    name: "Oprah Winfrey",
+    photo: "http://womenofrubies.com/wp-content/uploads/2017/12/oprah_winfrey_horizontal_a_l.jpg",
+    scores: [
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "4"
+    ]
+  },
+  {
+    name: "Roseanne Barr",
+    photo: "https://i.pinimg.com/736x/20/80/45/208045cd75cc45f9fa9c7a8a8305386c.jpg",
+    scores: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+{
+    name: "Miss Piggy",
+    photo: "https://twitter.com/misspiggy",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+{
+    name: "Miss Piggy",
+    photo: "https://pbs.twimg.com/profile_images/662349173467975680/UxlUTQL0_400x400.jpg",
+    scores: [
+      "5",
+      "4",
+      "3",
+      "2",
+      "1",
+      "5",
+      "4",
+      "3",
+      "2",
+      "1"
+    ]
+  }
+];
+module.exports = friends;
